fd3ce438cf769abdce0f306c4c0e0bd270f3e739
Durand D'souza <durand1@gmail.com>
2016-10-17 23:27:18 +0100

    Removed ipfs references from branch merge

--- static/js/ipfs.js
+++
@@ -1,8 +0,0 @@
-function use_ipfs(image) {
-    if (image.naturalWidth == 0 && image.src.search("/ipfs/") != -1) {
-        image.src = image.src.split('#')[1];
-    }
-}
-
-let image = document.getElementsByClassName("entry-image")[0].getElementsByTagName('img')[0];
-window.onload(use_ipfs(image));
